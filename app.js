bd2d4f5942b6bdca5f07b74f8319788cb2a484c9
SEBASTIAN HERNANDEZ VIVANCO <seba.hernandezv@duocuc.cl>
2024-05-17 11:04:37 -0400

    actualizacion 17-05-2024

--- app.js
+++ app.js
@@ -1,12 +1,34 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const morganBody = require("morgan-body");
+const loggerStream = require("./utils/handleLoger");
 const dbConnect = require("./config/mongo");
 const app = express();
 
 app.use(cors());
-app.use(express.json())
-app.use(express.static("storage"))
+app.use(express.json());
+app.use(express.static("storage"));
+
+// Este metodo es para trabajar en la consola solamente
+morganBody(app, {
+  
+});
+
+
+// -----------------------------//
+/**
+ * Esta seccion es para enviar el mensaje de error directo al Slack o cualquier otro sistema 
+ * como por ejemplo canal de telegra, estc.
+ */
+
+// morganBody(app, {
+//   noColors: true, // quitamos el color para que se envie a slak sin color
+//   stream: loggerStream, // le pasamos el loggerStream al morganBody para que lo envie a slak
+//   skip : function (req, res) {
+//     return res.statusCode < 400 // si la consulta no tiene error, omitira el envio al slack, ya que solo debe enviar errores para eso eso avisar en caso de errores 
+//   }
+// });
 
 const port = process.env.PORT || 3000;
 
---
+++ utils/handleLoger.js
@@ -0,0 +1,21 @@
+/**
+ * Esta seccion es para enviar el mensaje de error directo al Slack o cualquier otro sistema 
+ * como por ejemplo canal de telegra, estc.
+ */
+
+// const { IncomingWebhook } = require("@slack/webhook"); // paquete que crea la conección
+
+// const webHook = new IncomingWebhook (process.env.SLACK_WEHOOK) // instanciamos la url del slack
+
+
+// // con este metodo le decimos a la app que envie el dato a slak
+// const loggerStream = {
+//   write: message => { // message contien el LOG
+//     webHook.send({
+//       text:message
+//     })
+//     console.log("Capturando el LOG -> ", message);
+//   },
+// };
+
+// module.exports = loggerStream
